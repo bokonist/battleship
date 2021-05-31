7d2e608b6b6a63e03da2b5da66e05c59df9fe2c8
bokonist <jayrm1995@gmail.com>
2021-06-01 04:52:10 +0530

    wrote more tests for gameboard module

--- src/tests/GameBoard.test.ts
+++ src/tests/GameBoard.test.ts
@@ -2,21 +2,49 @@ import { GameBoard } from "../game/modules/GameBoard";
 import { ShipFactory } from "../game/factories/ShipFactory";
 import { Orientation } from "../game/typeDefinitions.d";
 
+afterEach(() => {
+  GameBoard.resetGrid();
+  GameBoard.clearShips();
+});
+
 it("default game board", () => {
-  let ship1 = ShipFactory(3, [0, 0], Orientation.SIDEWAYS);
-  expect(GameBoard.placeNewShip(ship1)).toBe(true);
+  let ship = ShipFactory(3, [0, 0], Orientation.SIDEWAYS);
+  expect(GameBoard.placeNewShip(ship)).toBe(true);
 });
 
-it("ship placements", () => {
-  let ship1 = ShipFactory(3, [49, 0], Orientation.SIDEWAYS);
-  expect(GameBoard.placeNewShip(ship1)).toBe(true);
-
-  let ship2 = ShipFactory(3, [49, 0], Orientation.UPRIGHT);
-  expect(GameBoard.placeNewShip(ship2)).toBe(false);
-
-  let ship3 = ShipFactory(3, [0, 49], Orientation.SIDEWAYS);
-  expect(GameBoard.placeNewShip(ship3)).toBe(false);
-
-  let ship4 = ShipFactory(10, [25, 25], Orientation.SIDEWAYS);
-  expect(GameBoard.placeNewShip(ship4)).toBe(true);
+it("ship placements - super large ship, sideways", () => {
+  let ship = ShipFactory(100, [25, 25], Orientation.SIDEWAYS);
+  expect(GameBoard.placeNewShip(ship)).toBe(false);
 });
+it("ship placements - super large ship, upright", () => {
+  let ship = ShipFactory(100, [25, 25], Orientation.UPRIGHT);
+  expect(GameBoard.placeNewShip(ship)).toBe(false);
+});
+it("ship placements - within bounds, sideways", () => {
+  let ship = ShipFactory(3, [49, 0], Orientation.SIDEWAYS);
+  expect(GameBoard.placeNewShip(ship)).toBe(true);
+});
+it("ship placements - out of bounds, sideways", () => {
+  let ship = ShipFactory(3, [0, 49], Orientation.SIDEWAYS);
+  expect(GameBoard.placeNewShip(ship)).toBe(false);
+});
+it("ship placements - within bounds, upright", () => {
+  let ship = ShipFactory(3, [25, 0], Orientation.UPRIGHT);
+  expect(GameBoard.placeNewShip(ship)).toBe(true);
+});
+it("ship placements - out of bounds, upright", () => {
+  let ship = ShipFactory(3, [49, 0], Orientation.UPRIGHT);
+  expect(GameBoard.placeNewShip(ship)).toBe(false);
+});
+
+it("gameboard - getShips() test", () => {
+  expect(GameBoard.getShips().length).toBe(0);
+  let ship = ShipFactory(3, [25, 0], Orientation.UPRIGHT);
+  let ship2 = ShipFactory(3, [49, 0], Orientation.SIDEWAYS);
+  GameBoard.placeNewShip(ship);
+  GameBoard.placeNewShip(ship2);
+  expect(GameBoard.getShips().length).toBe(2);
+});
+it("gameboard - ", () => {});
+it("placeholder", () => {});
+it("placeholder", () => {});
