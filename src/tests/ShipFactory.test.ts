d084e2ea86b5b378722d4d3879d3704e32759c00
bokonist <jayrm1995@gmail.com>
2021-06-01 01:04:23 +0530

    added few mode default ship tests

--- src/tests/ShipFactory.test.ts
+++ src/tests/ShipFactory.test.ts
@@ -1,8 +1,6 @@
 import { ShipFactory } from "../game/factories";
-enum Orientation {
-  SIDEWAYS,
-  UPRIGHT,
-}
+import { Orientation } from "../game/typeDefinitions.d";
+
 it("default ship settings", () => {
   let ship1 = ShipFactory(3, [0, 0], Orientation.SIDEWAYS);
   expect(ship1.body.length).toBe(3);
@@ -25,4 +23,26 @@ it("default ship settings", () => {
   expect(ship2.body).toEqual(expect.arrayContaining(expected2));
   expect(ship2.hitArray.length).toBe(0);
   expect(ship2.isSunk()).toBe(false);
+
+  let ship3 = ShipFactory(3, [1, 5], Orientation.UPRIGHT);
+  expect(ship2.body.length).toBe(3);
+  const expected3 = [
+    [1, 5],
+    [2, 5],
+    [3, 5],
+  ];
+  expect(ship3.body).toEqual(expect.arrayContaining(expected3));
+  expect(ship3.hitArray.length).toBe(0);
+  expect(ship3.isSunk()).toBe(false);
+
+  let ship4 = ShipFactory(3, [1, 5], Orientation.SIDEWAYS);
+  expect(ship4.body.length).toBe(3);
+  const expected4 = [
+    [1, 5],
+    [1, 6],
+    [1, 7],
+  ];
+  expect(ship4.body).toEqual(expect.arrayContaining(expected4));
+  expect(ship4.hitArray.length).toBe(0);
+  expect(ship4.isSunk()).toBe(false);
 });
