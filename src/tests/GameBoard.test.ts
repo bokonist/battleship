8a9996122bac2315ba9e5d66b73f67014b0803f0
bokonist <jayrm1995@gmail.com>
2021-06-05 02:54:44 +0530

    wrote tests for ship overlap, removeship and clear

--- src/tests/GameBoard.test.ts
+++ src/tests/GameBoard.test.ts
@@ -45,6 +45,45 @@ it("gameboard - getShips() test", () => {
   GameBoard.placeNewShip(ship2);
   expect(GameBoard.getShips().length).toBe(2);
 });
-it("gameboard - ", () => {});
+it("gameboard - remove ship", () => {
+  let ship = ShipFactory(3, [25, 0], Orientation.UPRIGHT);
+  let ship2 = ShipFactory(3, [49, 0], Orientation.SIDEWAYS);
+  GameBoard.placeNewShip(ship);
+  GameBoard.placeNewShip(ship2);
+  expect(GameBoard.getShips().length).toBe(2);
+  GameBoard.removeShip(ship2.id);
+  expect(GameBoard.getShips().length).toBe(1);
+});
+it("gameboard - conflicting ships sideways overlap", () => {
+  let ship = ShipFactory(3, [25, 0], Orientation.SIDEWAYS);
+  let ship2 = ShipFactory(3, [26, 0], Orientation.SIDEWAYS);
+  expect(GameBoard.placeNewShip(ship)).toBe(true);
+  expect(GameBoard.placeNewShip(ship2)).toBe(false);
+  expect(GameBoard.getShips().length).toBe(1);
+});
+it("gameboard - conflicting ships upright overlap", () => {
+  let ship = ShipFactory(3, [25, 25], Orientation.UPRIGHT);
+  let ship2 = ShipFactory(3, [26, 25], Orientation.UPRIGHT);
+  expect(GameBoard.placeNewShip(ship)).toBe(true);
+  expect(GameBoard.placeNewShip(ship2)).toBe(false);
+  expect(GameBoard.getShips().length).toBe(1);
+});
+it("gameboard - conflicting ships cross overlap", () => {
+  let ship = ShipFactory(3, [25, 25], Orientation.UPRIGHT);
+  let ship2 = ShipFactory(3, [26, 25], Orientation.SIDEWAYS);
+  expect(GameBoard.placeNewShip(ship)).toBe(true);
+  expect(GameBoard.placeNewShip(ship2)).toBe(false);
+  expect(GameBoard.getShips().length).toBe(1);
+});
+it("gameboard - clear ships", () => {
+  let ship = ShipFactory(3, [0, 25], Orientation.UPRIGHT);
+  let ship2 = ShipFactory(3, [26, 25], Orientation.SIDEWAYS);
+  expect(GameBoard.placeNewShip(ship)).toBe(true);
+  expect(GameBoard.placeNewShip(ship2)).toBe(true);
+  expect(GameBoard.getShips().length).toBe(2);
+  GameBoard.clearShips();
+  expect(GameBoard.getShips().length).toBe(0);
+});
+
 it("placeholder", () => {});
-it("placeholder", () => {});
+it("placeholder2", () => {});
