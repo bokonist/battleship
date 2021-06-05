c8505f12927eb5f5f0a9598d1d5a85a48876fa21
bokonist <jayrm1995@gmail.com>
2021-06-05 21:13:40 +0530

    calculate neighbor cells for all ships

--- src/game/factories/ShipFactory.ts
+++ src/game/factories/ShipFactory.ts
@@ -14,14 +14,43 @@ const ShipFactory = (
   let shipOrientation = orientation;
   let shipHitArray: [number, number][] = []; // will hold positions where ship is hit
   let shipBody: [number, number][] = [];
-  for (let i = 0; i < length; i++) {
-    if (orientation === Orientation.SIDEWAYS) {
-      shipBody.push([shipPosition[0], shipPosition[1] + i]);
-    } else if (orientation === Orientation.UPRIGHT) {
-      shipBody.push([shipPosition[0] + i, shipPosition[1]]);
+  let shipNeighbors: Set<string> = new Set();
+  const setup = () => {
+    for (let i = 0; i < length; i++) {
+      if (orientation === Orientation.SIDEWAYS) {
+        shipBody.push([shipPosition[0], shipPosition[1] + i]);
+      } else if (orientation === Orientation.UPRIGHT) {
+        shipBody.push([shipPosition[0] + i, shipPosition[1]]);
+      }
     }
-  }
+    const neighbors = [
+      [-1, -1],
+      [0, -1],
+      [1, -1],
+      [1, 0],
+      [1, 1],
+      [0, 1],
+      [-1, 1],
+      [-1, 0],
+    ]; // utility neighbors array to use to calculate 8 neighbors of a given cell
 
+    shipBody.forEach((cell) => {
+      let currentNeighbor = [0, 0];
+      currentNeighbor[0] = cell[0];
+      currentNeighbor[1] = cell[1];
+      neighbors.forEach((neighbor) => {
+        currentNeighbor[0] = neighbor[0] + cell[0];
+        currentNeighbor[1] = neighbor[1] + cell[1];
+        shipNeighbors.add(currentNeighbor.toString());
+        //    console.log(shipNeighbors);
+      });
+    });
+
+    shipBody.forEach((cell) => {
+      shipNeighbors.delete(cell.toString());
+    });
+  };
+  setup();
   const isSunk = () => {
     return shipHitArray.length === length;
   };
@@ -30,6 +59,7 @@ const ShipFactory = (
     length: shipLength,
     position: shipPosition,
     body: shipBody,
+    neighbors: shipNeighbors,
     orientation: shipOrientation,
     isSunk: isSunk,
     hitArray: shipHitArray,
