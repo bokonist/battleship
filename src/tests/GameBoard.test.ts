4ca487795163cf8ca09659195461ee4d6f1d19f0
bokonist <jayrm1995@gmail.com>
2021-06-08 11:22:59 +0530

    wrote tests for random config

--- src/tests/GameBoard.test.ts
+++ src/tests/GameBoard.test.ts
@@ -13,42 +13,42 @@ it("default game board", () => {
 });
 describe("ship placements", () => {
   it("ship placements - super large ship, sideways", () => {
-    let ship = ShipFactory(100, [25, 25], Orientation.SIDEWAYS);
+    let ship = ShipFactory(100, [5, 5], Orientation.SIDEWAYS);
     expect(GameBoard.placeNewShip(ship)).toBe(false);
   });
   it("ship placements - super large ship, upright", () => {
-    let ship = ShipFactory(100, [25, 25], Orientation.UPRIGHT);
+    let ship = ShipFactory(100, [5, 5], Orientation.UPRIGHT);
     expect(GameBoard.placeNewShip(ship)).toBe(false);
   });
   it("ship placements - within bounds, sideways", () => {
-    let ship = ShipFactory(3, [49, 0], Orientation.SIDEWAYS);
+    let ship = ShipFactory(3, [9, 0], Orientation.SIDEWAYS);
     expect(GameBoard.placeNewShip(ship)).toBe(true);
   });
   it("ship placements - out of bounds, sideways", () => {
-    let ship = ShipFactory(3, [0, 49], Orientation.SIDEWAYS);
+    let ship = ShipFactory(3, [0, 9], Orientation.SIDEWAYS);
     expect(GameBoard.placeNewShip(ship)).toBe(false);
   });
   it("ship placements - within bounds, upright", () => {
-    let ship = ShipFactory(3, [25, 0], Orientation.UPRIGHT);
+    let ship = ShipFactory(3, [5, 0], Orientation.UPRIGHT);
     expect(GameBoard.placeNewShip(ship)).toBe(true);
   });
   it("ship placements - out of bounds, upright", () => {
-    let ship = ShipFactory(3, [49, 0], Orientation.UPRIGHT);
+    let ship = ShipFactory(3, [9, 0], Orientation.UPRIGHT);
     expect(GameBoard.placeNewShip(ship)).toBe(false);
   });
 });
 describe("add and remove ships", () => {
   it("gameboard - getShips() test", () => {
     expect(GameBoard.getShips().length).toBe(0);
-    let ship = ShipFactory(3, [25, 0], Orientation.UPRIGHT);
-    let ship2 = ShipFactory(3, [49, 0], Orientation.SIDEWAYS);
+    let ship = ShipFactory(3, [5, 0], Orientation.UPRIGHT);
+    let ship2 = ShipFactory(3, [9, 4], Orientation.SIDEWAYS);
     GameBoard.placeNewShip(ship);
     GameBoard.placeNewShip(ship2);
     expect(GameBoard.getShips().length).toBe(2);
   });
   it("gameboard - remove ship", () => {
-    let ship = ShipFactory(3, [25, 0], Orientation.UPRIGHT);
-    let ship2 = ShipFactory(3, [49, 0], Orientation.SIDEWAYS);
+    let ship = ShipFactory(3, [5, 0], Orientation.UPRIGHT);
+    let ship2 = ShipFactory(3, [9, 4], Orientation.SIDEWAYS);
     GameBoard.placeNewShip(ship);
     GameBoard.placeNewShip(ship2);
     expect(GameBoard.getShips().length).toBe(2);
@@ -58,30 +58,30 @@ describe("add and remove ships", () => {
 });
 describe("conflicting ships tests", () => {
   it("gameboard - conflicting ships sideways overlap", () => {
-    let ship = ShipFactory(3, [25, 0], Orientation.SIDEWAYS);
-    let ship2 = ShipFactory(3, [26, 0], Orientation.SIDEWAYS);
+    let ship = ShipFactory(3, [5, 5], Orientation.SIDEWAYS);
+    let ship2 = ShipFactory(3, [5, 4], Orientation.SIDEWAYS);
     expect(GameBoard.placeNewShip(ship)).toBe(true);
     expect(GameBoard.placeNewShip(ship2)).toBe(false);
     expect(GameBoard.getShips().length).toBe(1);
   });
   it("gameboard - conflicting ships upright overlap", () => {
-    let ship = ShipFactory(3, [25, 25], Orientation.UPRIGHT);
-    let ship2 = ShipFactory(3, [26, 25], Orientation.UPRIGHT);
+    let ship = ShipFactory(3, [5, 5], Orientation.UPRIGHT);
+    let ship2 = ShipFactory(3, [6, 5], Orientation.UPRIGHT);
     expect(GameBoard.placeNewShip(ship)).toBe(true);
     expect(GameBoard.placeNewShip(ship2)).toBe(false);
     expect(GameBoard.getShips().length).toBe(1);
   });
   it("gameboard - conflicting ships cross overlap", () => {
-    let ship = ShipFactory(3, [25, 25], Orientation.UPRIGHT);
-    let ship2 = ShipFactory(3, [26, 25], Orientation.SIDEWAYS);
+    let ship = ShipFactory(3, [5, 5], Orientation.UPRIGHT);
+    let ship2 = ShipFactory(3, [6, 4], Orientation.SIDEWAYS);
     expect(GameBoard.placeNewShip(ship)).toBe(true);
     expect(GameBoard.placeNewShip(ship2)).toBe(false);
     expect(GameBoard.getShips().length).toBe(1);
   });
 });
 it("gameboard - clear ships", () => {
-  let ship = ShipFactory(3, [0, 25], Orientation.UPRIGHT);
-  let ship2 = ShipFactory(3, [26, 25], Orientation.SIDEWAYS);
+  let ship = ShipFactory(3, [0, 5], Orientation.UPRIGHT);
+  let ship2 = ShipFactory(3, [6, 5], Orientation.SIDEWAYS);
   expect(GameBoard.placeNewShip(ship)).toBe(true);
   expect(GameBoard.placeNewShip(ship2)).toBe(true);
   expect(GameBoard.getShips().length).toBe(2);
@@ -90,24 +90,24 @@ it("gameboard - clear ships", () => {
 });
 describe("ship hit tests", () => {
   it("size 3 ship upright hit tests", () => {
-    let ship = ShipFactory(3, [25, 25], Orientation.UPRIGHT);
+    let ship = ShipFactory(3, [5, 5], Orientation.UPRIGHT);
     expect(GameBoard.placeNewShip(ship)).toBe(true);
     expect(GameBoard.aliveShipCount()).toBe(1);
     GameBoard.hit(ship.body[0]);
     GameBoard.hit(ship.body[1]);
     GameBoard.hit(ship.body[2]);
     expect(GameBoard.aliveShipCount()).toBe(0);
-    expect(GameBoard.safeCellCount()).toBe(2500 - 3 - 12);
+    expect(GameBoard.safeCellCount()).toBe(100 - 3 - 12);
   });
   it("size 3 ship sideways hit tests", () => {
-    let ship = ShipFactory(3, [25, 25], Orientation.SIDEWAYS);
+    let ship = ShipFactory(3, [5, 5], Orientation.SIDEWAYS);
     expect(GameBoard.placeNewShip(ship)).toBe(true);
     expect(GameBoard.aliveShipCount()).toBe(1);
     GameBoard.hit(ship.body[0]);
     GameBoard.hit(ship.body[1]);
     GameBoard.hit(ship.body[2]);
     expect(GameBoard.aliveShipCount()).toBe(0);
-    expect(GameBoard.safeCellCount()).toBe(2500 - 3 - 12);
+    expect(GameBoard.safeCellCount()).toBe(100 - 3 - 12);
   });
   it("size 3 ship, edge of board, sideways hit tests", () => {
     let ship = ShipFactory(3, [0, 0], Orientation.SIDEWAYS);
@@ -117,7 +117,7 @@ describe("ship hit tests", () => {
     GameBoard.hit(ship.body[1]);
     GameBoard.hit(ship.body[2]);
     expect(GameBoard.aliveShipCount()).toBe(0);
-    expect(GameBoard.safeCellCount()).toBe(2500 - 3 - 5);
+    expect(GameBoard.safeCellCount()).toBe(100 - 3 - 5);
   });
   it("size 3 ship, edge of board, upright hit tests", () => {
     let ship = ShipFactory(3, [0, 0], Orientation.UPRIGHT);
@@ -128,7 +128,7 @@ describe("ship hit tests", () => {
     GameBoard.hit(ship.body[1]);
     GameBoard.hit(ship.body[2]);
     expect(GameBoard.aliveShipCount()).toBe(0);
-    expect(GameBoard.safeCellCount()).toBe(2500 - 3 - 5);
+    expect(GameBoard.safeCellCount()).toBe(100 - 3 - 5);
   });
 });
 describe("missed hit tests", () => {
@@ -140,5 +140,12 @@ describe("missed hit tests", () => {
     expect(GameBoard.hit([0, 3])).toBe(HitResults.MISSED);
   });
 });
+describe("random ship setup", () => {
+  it("random setup", () => {
+    expect(GameBoard.randomSetup()).toBe(true);
+    expect(GameBoard.getShips().length).toBe(10);
+  });
+});
+
 it("placeholder", () => {});
 it("placeholder2", () => {});
